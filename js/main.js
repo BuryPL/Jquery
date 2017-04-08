706854d016dec8b9c62a0f1701c1cbe3353df089
Krzysztof Burkot <buryPL@gmail.com>
2017-04-08 14:23:56 +0200

    JQuery_101_2

--- js/main.js
+++ js/main.js
@@ -2,13 +2,22 @@ $(function(){
     'use strict';
     
     $('#first').css({color:'red'});
-    //alert($('#first').text());
-    $('#first').text('Hello World');
+    //alert($('#first').text()); //zwraca wartość #first
+    $('#first').text('Hello World'); //ustawia wartość #first
     
     //alert($('#second').html());
     
     //alert($('#second').html('<b>Cześć</b>'));
-    $('#second').before('<p>Przykład</p>');
+    $('#second').before('<p id="paragraf">Przykład</p>');
+    $('#paragraf').css({color:'red'});
+    
+//    $('li').each(function(){
+//        alert($(this).text());
+//    });
+    
+    $('#przycisk').click(function(){
+        alert($(this).text());
+    });
 })
 
 
